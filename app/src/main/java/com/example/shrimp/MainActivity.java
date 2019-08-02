0c9ac17b4f4e22d41b455f5292b3c9b52e79e0b8
balagos <moca104hotm@gmail.com>
2019-08-02 16:01:36 +0700

    change login form

--- app/src/main/java/com/example/shrimp/MainActivity.java
+++ app/src/main/java/com/example/shrimp/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.shrimp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    public  String  a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,15 +22,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void Login(View view) {
-        EditText textView =  findViewById(R.id.textView);
-        TextView textView1 = findViewById(R.id.textView3);
-        TextView textView2 = findViewById(R.id.textView4);
-        EditText editText = findViewById(R.id.textView1);
 
-        textView1.setText(textView.getText());
-        textView2.setText(editText.getText());
+
+    public void alertDialog() {
+        if (a.equals("number")) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setMessage("ID shouldn't  only number");
+            dialog.setTitle("     Stop!!!!");
+            dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(getApplicationContext(),"Chang your ID",Toast.LENGTH_LONG).show();
+                }
+            });
+            AlertDialog alertDialog = dialog.create();
+            alertDialog.show();
+        }if(a.equals("IdOrPass")){
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle("     Stop!!!!");
+            dialog.setMessage("Id or Passord wrong");
+            dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(getApplicationContext(),"Chang your ID",Toast.LENGTH_LONG).show();
+                }
+            });
+            AlertDialog alertDialog=dialog.create();
+            alertDialog.show();
+
+        }
+    }
+    public void Login(View view) {
+
+        TextInputEditText textId  =  findViewById(R.id.textView);
+        TextInputEditText textPass =  findViewById(R.id.textView1);
+
+        if(android.text.TextUtils.isDigitsOnly(textId.getText()) && !android.text.TextUtils.equals(textId.getText(),"")){
+            a = "number";
+            alertDialog();
+        }
+        if(textId.getText().length() < 4  || textPass.getText().length() < 8 ){
+            a = "IdOrPass";
+            alertDialog();
+        }
+
 
 
     }
+
+
 }
